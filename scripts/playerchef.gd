86b08d4e53c2f0dedac9a20e0b621908b2289493
Mr Coconut <bradley@sellers-blais.com>
2026-03-07 11:28:11 -0800

    Added the actual assets

--- scripts/playerchef.gd
+++ scripts/playerchef.gd
@@ -2,13 +2,16 @@ extends CharacterBody2D
 
 @export var speed = 400
 
+func _ready() -> void:
+	$Label.add_theme_constant_override("shadow_outline_size", 20)
+
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
 	velocity = velocity.normalized() * speed
 	# Flip player as they move
 	if velocity.y > 0 or velocity.x > 0 or velocity.y < 0 or velocity.x < 0:
-		rotation = atan2(velocity.x, velocity.y)
+		$AnimatedSprite2D.rotation = atan2(velocity.x, velocity.y)
 func _physics_process(delta):
 	# Animation logic
 	if velocity.y != 0 or velocity.x != 0:
