1211ee289945551e123611c7fbbc0d7069f74b85
Mr Coconut <bradley@sellers-blais.com>
2026-03-10 19:42:40 -0700

    New yeast

--- autoloads/global.gd
+++ autoloads/global.gd
@@ -12,7 +12,7 @@ var random_food : int = randi_range(0, 2)
 
 var baguette_recipe : String = "Recipe: \n1: Mix Wheat, Water, Salt, and Yeast in a bowl \n2: Bake in oven \n3: Take to delivery point"
 var coffee_recipe : String = "Recipe: \n1: Put Coffee Packet and Water in Coffee Machine \n2: Wait for Coffee Machine to finish \n3: Take Coffee to delivery point"
-var croissant_recipe : String = "Recipe: \n1: Mix Milk, Butter, Yeast, Salt, and Flour in bowl \n2:Cut on chopping board \n3: Bake in oven \n4: Take to delivery point" 
+var croissant_recipe : String = "Recipe: \n1: Mix Milk, Butter, Yeast, Salt, and Wheat in bowl \n2:Cut on chopping board \n3: Bake in oven \n4: Take to delivery point" 
 
 func _ready() -> void:
 	print(random_food)
